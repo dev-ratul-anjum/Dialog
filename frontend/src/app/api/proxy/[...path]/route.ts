d9fa6cd619302944907c8c5630eb489c20e7d324
dev-ratul-anjum <206848890+dev-ratul-anjum@users.noreply.github.com>
2026-02-07 12:19:48 +0600

    fix: auto-delete unused cache after 5min on /rooms page

--- frontend/src/app/api/proxy/[...path]/route.ts
+++ frontend/src/app/api/proxy/[...path]/route.ts
@@ -1,4 +1,3 @@
-// app/api/proxy/[...path]/route.ts
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -11,30 +10,18 @@ export async function GET(request: NextRequest) {
   let cookieHeader = cookieStore.toString();
   cookieHeader = decodeURIComponent(cookieHeader); // Decode for signed cookie
 
-  console.log("fullPath ", fullPath);
-
   try {
     const backendResponse = await fetch(fullPath, {
-      method: "GET", // Or dynamic: request.method
+      method: "GET",
       headers: {
         Cookie: cookieHeader,
-        // Add other headers if needed, e.g., 'Content-Type': 'application/json'
       },
     });
 
     const data = await backendResponse.json();
 
-    console.log("fullPath data", data);
-
     return NextResponse.json(data, { status: backendResponse.status });
   } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
+    return NextResponse.json({ error: "Proxy error" }, { status: 500 });
   }
 }
-
-// POST/PUT/DELETE support যদি লাগে
-export async function POST(request: NextRequest) {
-  // Similar as GET, but add body: await request.body()
-  // ...
-}
-// অন্য methods add করুন যা লাগে
--- frontend/src/app/rooms/components/ChatList.tsx
+++ frontend/src/app/rooms/components/ChatList.tsx
@@ -1,24 +1,15 @@
 "use client";
+
 import { useInfiniteQuery } from "@tanstack/react-query";
-import {
-  CheckCheck,
-  FileText,
-  ListFilter,
-  Search,
-  SquarePen,
-  UserIcon,
-  VolumeX,
-} from "lucide-react";
-import Image from "next/image";
+import { ListFilter, Search, SquarePen } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
-import { List, RowComponentProps } from "react-window";
-import SearchSkeleton from "./SearchSkeleton";
+import { List } from "react-window";
 import ChatRowSkeleton from "./ChatRowSkeleton";
-import HeaderSkeleton from "./HeaderSkeleton";
 import FiltersSkeleton from "./FiltersSkeleton";
 import useDebounce from "@/hooks/useDebounce";
 import NoConversationSkeleton from "./NoConversationSkeleton";
+import Conversation from "./Conversation";
 
 const ChatList = () => {
   const [selectedChatId, setSelectedChatId] = useState<string | null>(null);
@@ -27,10 +18,11 @@ const ChatList = () => {
 
   const debouncedSearchChange = useDebounce((value: string) => {
     setDebouncedSearch(value);
-  }, 400);
+  }, 500);
 
   const {
     data,
+    isLoading,
     fetchNextPage,
     fetchPreviousPage,
     isFetchingNextPage,
@@ -57,11 +49,12 @@ const ChatList = () => {
       if (!firstPage.data.meta.hasPrevPage) return undefined;
       return firstPage.data.meta.prevPage;
     },
+    staleTime: 5_000,
+    gcTime: 5 * 60_000,
   });
 
   const items = data?.pages.flatMap((page) => page.data.conversations) || [];
-  const totalRow = data?.pages[0].data.meta.totalConversations || 0;
-  const isInitialLoading = !data;
+  const totalRow = items.length;
 
   const onRowsRender = ({
     startIndex,
@@ -78,101 +71,46 @@ const ChatList = () => {
     }
   };
 
-  const conversation = ({ index, style }: RowComponentProps) => {
-    const isSelected = items[index].id === selectedChatId;
-    return (
-      <Link
-        href={`/rooms/${items[index].id}`}
-        style={style}
-        onClick={() => setSelectedChatId(items[index].id)}
-        className={`flex cursor-pointer items-center gap-3 border-b border-[#e9edef]/50 px-3 py-3 hover:bg-[#f0f2f5] transition ${isSelected && "bg-[#f0f2f5] group relative"}`}
-      >
-        {items[index].participantPhoto ? (
-          <Image
-            src={items[index].participantPhoto}
-            alt="User"
-            width={48}
-            height={48}
-            className="rounded-full object-cover"
-          />
-        ) : (
-          <div className="flex h-12 w-12 items-center justify-center rounded-full bg-gray-300 text-white">
-            <UserIcon className="h-6 w-6" />
-          </div>
-        )}
-
-        <div className="min-w-0 flex-1">
-          <div className="flex items-baseline justify-between">
-            <h3 className="truncate text-base font-medium text-[#111b21]">
-              {items[index].participantName}
-            </h3>
-            <span className="text-xs text-[#667781]">
-              {items[index].lastMessageAt}
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <p className="flex items-center gap-1 truncate text-sm text-[#667781]">
-              {items[index].attachments && (
-                <FileText className="h-4 w-4 text-red-500" />
-              )}
-              <span>
-                {items[index].lastMessage && items[index].lastMessage}
-              </span>
-            </p>
-          </div>
-        </div>
-      </Link>
-    );
-  };
-
   return (
     <aside
       id="chat-list-panel"
       className="absolute z-10 flex h-full w-full flex-col border-r border-[#e9edef] bg-white transition-transform duration-300 md:relative md:w-87.5 lg:w-100"
     >
       {/* Header */}
-      {isInitialLoading ? (
-        <HeaderSkeleton />
-      ) : (
-        <header className="flex h-15 items-center justify-between bg-white px-4 py-2">
-          <h1 className="text-2xl font-bold tracking-tight">Chats</h1>
-          <div className="flex gap-3 text-[#54656f]">
-            <button>
-              <Link prefetch={false} href="/add-chat">
-                <SquarePen className="h-5 w-5" />
-              </Link>
-            </button>
-            <button>
-              <ListFilter className="h-5 w-5" />
-            </button>
-          </div>
-        </header>
-      )}
+      <header className="flex h-15 items-center justify-between bg-white px-4 py-2">
+        <h1 className="text-2xl font-bold tracking-tight">Chats</h1>
+        <div className="flex gap-3 text-[#54656f]">
+          <button>
+            <Link prefetch={false} href="/add-chat">
+              <SquarePen className="h-5 w-5" />
+            </Link>
+          </button>
+          <button>
+            <ListFilter className="h-5 w-5" />
+          </button>
+        </div>
+      </header>
 
       {/* Search Bar */}
-      {isInitialLoading ? (
-        <SearchSkeleton />
-      ) : (
-        <div className="px-3 pb-2">
-          <div className="flex h-9 items-center rounded-lg bg-[#f0f2f5] px-3">
-            <Search className="h-5 w-5 cursor-pointer text-[#54656f]" />
-            <input
-              type="text"
-              placeholder="Search or start a new chat"
-              value={query}
-              onChange={(e) => {
-                const text = e.target.value;
-                setQuery(text);
-                debouncedSearchChange(text);
-              }}
-              className="ml-4 w-full border-none bg-transparent text-sm outline-none placeholder-[#667781]"
-            />
-          </div>
+      <div className="px-3 pb-2">
+        <div className="flex h-9 items-center rounded-lg bg-[#f0f2f5] px-3">
+          <Search className="h-5 w-5 cursor-pointer text-[#54656f]" />
+          <input
+            type="text"
+            placeholder="Search or start a new chat"
+            value={query}
+            onChange={(e) => {
+              const text = e.target.value;
+              setQuery(text);
+              debouncedSearchChange(text);
+            }}
+            className="ml-4 w-full border-none bg-transparent text-sm outline-none placeholder-[#667781]"
+          />
         </div>
-      )}
+      </div>
 
       {/* Filters */}
-      {isInitialLoading ? (
+      {isLoading ? (
         <FiltersSkeleton />
       ) : (
         <div className="scrollbar-hide flex gap-2 overflow-x-auto px-3 py-2">
@@ -193,15 +131,19 @@ const ChatList = () => {
 
       {/* Chat List Items */}
       <div className="custom-scrollbar flex-1 overflow-y-auto">
-        {isInitialLoading ? (
+        {isLoading ? (
           Array.from({ length: 8 }).map((_, i) => <ChatRowSkeleton key={i} />)
         ) : (
           <>
             <List
-              rowComponent={conversation}
+              rowComponent={Conversation}
               rowCount={totalRow}
               rowHeight={73}
-              rowProps={{}}
+              rowProps={{
+                items,
+                selectedChatId,
+                setSelectedChatId,
+              }}
               onRowsRendered={onRowsRender}
               overscanCount={10}
             />
@@ -215,7 +157,7 @@ const ChatList = () => {
           </>
         )}
 
-        {items.length === 0 && !isInitialLoading && <NoConversationSkeleton />}
+        {items.length === 0 && !isLoading && <NoConversationSkeleton />}
       </div>
     </aside>
   );
